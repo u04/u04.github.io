fa3e125105401c6f2365f193b30ecceef29cf1d9
cdey1 <300920267@cencol.ca>
2018-05-22 12:58:21 -0400

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,72 @@
+/* Custom JS goes here */
+
+// IIFE - Immediately Invoked Function Expression
+// anonymous self executing function
+(function () {
+    "use strict"
+
+    // About Button Click event handler
+    function AboutButtonClick() {
+        console.log("About Button Clicked!");
+    }
+
+    function AboutButtonOver(event) {
+        event.currentTarget.style.opacity = 0.3;
+    }
+
+    function AboutButtonOut(event) {
+        event.currentTarget.style.opacity = 1.0;
+    }
+
+    function HomeContent() {
+        let AboutButton = document.getElementById("AboutButton");
+
+        AboutButton.textContent = "About";
+
+        // About Button event listener
+        AboutButton.addEventListener("click", AboutButtonClick);
+        AboutButton.addEventListener("mouseover", AboutButtonOver);
+        AboutButton.addEventListener("mouseout", AboutButtonOut);
+    }
+
+    function ReturnPI() {
+        return Math.PI;
+    }
+
+    function AboutContent() {
+        let myNumber = ReturnPI();
+        console.log("My Funky Number: " + myNumber);
+
+        let paragraph = document.getElementById("paragraph");
+
+        let mySentence = "Words such as 'gosh' & 'heck' are less offensive, okbreyeyeyeterfwerbhyrtyrbzzzz";
+
+        paragraph.textContent = mySentence;
+    }
+
+    function Start() {
+        // local variable
+        let title = document.title;
+
+        console.log("App Started!");
+        console.log("----------------------------");
+        console.log("Title: " + title);
+
+        switch (title) {
+            case "COMP125 - Home":
+                HomeContent();
+                break;
+
+            case "COMP125 - About":
+                AboutContent();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    //window.onload = Start;
+
+    window.addEventListener("load", Start);
+})();
