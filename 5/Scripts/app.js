ed8a1dc2a7de5d0f183f38652484f20e3b51907a
u04 <chowkas@rogers.com>
2018-07-26 21:21:16 -0400

    --

--- 5/Scripts/app.js
+++ 5/Scripts/app.js
@@ -2,27 +2,53 @@
 
 // IIFE - Immediately Invoked Function Expression
 // anonymous self executing function
+"use strict";
+(function() {
+  function Start() {
+    console.log("app startred");
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open("get", ".5/paragraphs.json");
+    httpRequest.send(null);
 
-(function () {
 
-    function jsonFileReadByAjax(){
+    function jsonFileReadByAjax() {
+      console.log("started function jsonFileReadByAjax");
+      //let httpRequest = new XMLHttpRequest();
+      httpRequest.onreadystatechange = iAmReady;
 
+      function iAmReady() {
+        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+          console.log("using json before redbyAjaxFuncation");
+          let jsonFile = JSON.parse(httpRequest.responseText);
+          document.getElementById("idOfProjectTextz").innerHTML = jsonFile.paraFromJson[1];
+          document.getElementById("idOfProjectTextk").innerHTML = jsonFile.paraFromJson[2];
+        }
+        console.log("botto");
+        //httpRequest.open("get", ".5/paragraphs.json");
+        //httpRequest.send(null);
+      }
     }
 
-
-    
-    "use strict"
     let title = document.title;
     switch (title) {
-        case "About Me":
-            let paragraphMe = document.getElementById("textInMainPage");
-            let myTextAboutMe = "I am Dey. I don't want to write my full name here to make it harder for someone serching the web to link this page to me. Right now my mission is to make this webpage.";
-            paragraphMe.textContent = myTextAboutMe;
-            let titleMe = document.getElementById("meInMainPage");
-            let titleMeText = "me";
-            titleMe.textContent = titleMeText;
-            break;
-        case "Projects":
+      case "About Me":
+        let paragraphMe = document.getElementById("textInMainPage");
+        let myTextAboutMe =
+          "I am Dey. I don't want to write my full name here to make it harder for someone serching the web to link this page to me. Right now my mission is to make this webpage.";
+        paragraphMe.textContent = myTextAboutMe;
+        let titleMe = document.getElementById("meInMainPage");
+        let titleMeText = "me";
+        titleMe.textContent = titleMeText;
+        break;
+      case "Projects":
+        console.log("switching");
+        jsonFileReadByAjax();
+      /*
+        console.log("in project of switch");
+        let proj = document.getElementById("idOfProjectText");
+        proj.textContent = usingJson();
+
+        /*
             let proj = document.getElementById("idOfProjectText");
             let projText = "having a github account";
             proj.textContent = projText;
@@ -33,23 +59,31 @@
             let textzf = "Hooking onto HTMLElements";
             projzk.textContent = textzf;
             break;
-        case "Contact":
-            var fname = document.getElementById("FirstName");
-            var lname = document.getElementById("LastName");
-            var email = document.getElementById("EmailAddress");
-            var pnumber = document.getElementById("PhoneNumber");
-            var message = document.getElementById("Message");
-            function DisplayTextToConsole() {
-                console.log("first name:" + fname.value);
-                console.log("last nsme: " + lname.value);
-                console.log("email: " + email.value);
-                console.log("phone number: " + pnumber.value);
-                console.log("message: " + message.value);
-            }
-            function DirectToHomepage() {
-                location.href = "./index.html"
-            }
-            document.getElementById("submit").addEventListener("click", DisplayTextToConsole);
-            document.getElementById("cancel").addEventListener("click", DirectToHomepage);
-    };
+*/
+      case "Contact":
+        var fname = document.getElementById("FirstName");
+        var lname = document.getElementById("LastName");
+        var email = document.getElementById("EmailAddress");
+        var pnumber = document.getElementById("PhoneNumber");
+        var message = document.getElementById("Message");
+        function DisplayTextToConsole() {
+          console.log("first name:" + fname.value);
+          console.log("last nsme: " + lname.value);
+          console.log("email: " + email.value);
+          console.log("phone number: " + pnumber.value);
+          console.log("message: " + message.value);
+        }
+        function DirectToHomepage() {
+          location.href = "./index.html";
+        }
+        document
+          .getElementById("submit")
+          .addEventListener("click", DisplayTextToConsole);
+        document
+          .getElementById("cancel")
+          .addEventListener("click", DirectToHomepage);
+    }
+  }
+
+  window.addEventListener("load", Start);
 })();
